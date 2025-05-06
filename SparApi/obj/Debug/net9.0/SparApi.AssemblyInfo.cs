fad8b810928fddd5e3e021590bba5c006ad0d360
Mphiliseni <Mphilisenialexmhlongo@gmail.com>
2025-05-06 21:26:04 +0200

    Update assembly informational version and cache

--- SparApi/obj/Debug/net9.0/SparApi.AssemblyInfo.cs
+++ SparApi/obj/Debug/net9.0/SparApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SparApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3edf2cbe8ba9764beeb8d9aa4b7b50b464385058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad6f4afaac55044f9f03e91cc794ce0c1e56e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SparApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SparApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
